3d52a7727be78e02bef748b282d343d24d6f857a
Noah <noah.endale@gmail.com>
2018-10-07 23:39:55 -0400

    Created testing components

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,1 +1,33 @@
-console.log('aye');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class SearchBar extends React.Component {
+	render() {
+		return (
+			<input type="text" placeholder="Enter your search term" name=""/>
+		)
+	}
+}
+
+class Fuckery extends React.Component {
+	render(){
+		return (
+			<input type="submit"/>
+		)
+	}
+}
+
+class App extends React.Component{
+	render() {
+		return (
+			<div>
+				<h1>Git Some</h1>
+				<SearchBar />
+				<Fuckery />
+			</div>
+		)
+	}
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
+
